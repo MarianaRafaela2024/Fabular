935ed3db62cdf355b9a3575d7fde2bdab9eccb4f
Thamires Bonezo <thamiresbonezo@gmail.com>
2026-07-18 16:16:56 -0300

    inicio da notificação

---
+++ backend/API-Fabular/Contracts/NotificacaoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Fabular.Contracts;
+
+public class NotificacaoRequest
+{
+    public string Nome { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string Hora { get; set; } = "";
+}
---
+++ backend/API-Fabular/Controllers/NotificacaoController.cs
@@ -0,0 +1,41 @@
+﻿using API_Fabular.Contracts;
+using API_Fabular.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Fabular.Controllers;
+
+[ApiController]
+[Route("api/contato")]
+public class NotificacaoController : ControllerBase
+{
+    private readonly BrevoService _brevoService;
+
+    public NotificacaoController(
+        BrevoService brevoService)
+    {
+        _brevoService = brevoService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Enviar(
+        [FromBody] ContatoRequest request)
+    {
+        try
+        {
+            var sucesso =
+                await _brevoService.EnviarContato(
+                    request.Nome,
+                    request.Email,
+                    request.Hora);
+
+            return Ok(new
+            {
+                sucesso
+            });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.ToString());
+        }
+    }
+}
