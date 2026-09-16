feda1e12ef1a4d94b09cb0542c7fa003ed538cc4
mariana <marianarafa09@gmail.com>
2026-09-16 09:35:42 -0300

    adicionado itens no banco de emoji

--- js/models/stories.js
+++ js/models/stories.js
@@ -826,7 +826,7 @@ const EMOJI_POR_PALAVRA = {
   tesouro: '💎', mapa: '🗺️', chave: '🔑', porta: '🚪',
   janela: '🪟', cama: '🛏️', travesseiro: '🛏️', cobertor: '🛏️',
   chapeu: '🎩', sapato: '👟', roupa: '👕', vestido: '👗',
-  oculos: '👓', relogio: '⌚', presente: '🎁',
+  oculos: '👓', relogio: '⌚', presente: '🎁', avermelhadas: '🔴', corais: '🪸', vibrantes: '📳', tentáculos: '🐙',
   musica: '🎵', instrumento: '🎸', piano: '🎹', violao: '🎸',
   teatro: '🎭', cinema: '🎬', camera: '📷', televisao: '📺',
   computador: '💻', tablet: '📱', robo: '🤖', pingo: '💧',
